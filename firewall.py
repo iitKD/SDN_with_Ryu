a0485d3e17171b7726ee50f2053f2b209cb7c246
iitKD <valar5736@gmail.com>
2023-11-02 17:48:19 +0530

    mit

--- firewall.py
+++ firewall.py
@@ -42,9 +42,9 @@ class FirewallMonitor(app_manager.RyuApp):
         actions = []
 
         # Drop the packets that match the firewall rules
-        self.add_flow(datapath, 1, match_h2_to_h5, actions)
-        self.add_flow(datapath, 1, match_h3_to_h5, actions)
-        self.add_flow(datapath, 1, match_h1_to_h4, actions)
+        self.add_flow(datapath, 0, match_h2_to_h5, actions)
+        self.add_flow(datapath, 0, match_h3_to_h5, actions)
+        self.add_flow(datapath, 0, match_h1_to_h4, actions)
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
@@ -61,17 +61,29 @@ class FirewallMonitor(app_manager.RyuApp):
         datapath.send_msg(mod)
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",ev.msg.msg_len, ev.msg.total_len)
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
 
         pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocol(ethernet.ethernet)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
 
-        # Count packets coming from host H3 on switch S1
-        if datapath.id == 1 and eth.src == '00:00:00:00:00:03':
-            self.packet_count += 1
-            self.logger.info(f"Packet count from H3 on switch S1: {self.packet_count}")
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            return
+        self.logger.info("packet in %s %s %s %s", datapath.id, eth.src, eth.dst, in_port)
+        out_port = ofproto.OFPP_FLOOD
+        actions = [parser.OFPActionOutput(out_port)]
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
 
     
 
