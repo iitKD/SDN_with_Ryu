86d1b8eff72d6fbf08e7a918267cf15f96bc75fd
iitKD <valar5736@gmail.com>
2023-11-03 16:15:54 +0530

    commit

--- firewall.py
+++ firewall.py
@@ -35,98 +35,43 @@ class FirewallMonitor(app_manager.RyuApp):
         #     eth_dst='00:00:00:00:00:04'   # H4 MAC address
         # )
 
-        actions = []
+        # actions = []
 
-        # Drop the packets that match the firewall rules
+        # # Drop the packets that match the firewall rules
+        # self.add_flow(datapath, 0, match_h2_to_h5, actions)
+        # self.add_flow(datapath, 0, match_h5_to_h2, actions)
+        actions = [parser.OFPActionOutput(ofproto.OFPPC_NONE, 0)]
+
         self.add_flow(datapath, 0, match_h2_to_h5, actions)
         self.add_flow(datapath, 0, match_h5_to_h2, actions)
+
         #self.add_flow(datapath, 1, match_h1_to_h4, actions)
-    # def add_flow(self, datapath, priority, match, actions):
-    #     ofproto = datapath.ofproto
-    #     parser = datapath.ofproto_parser
-
-    #     inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
-    #     mod = parser.OFPFlowMod(
-    #         datapath=datapath,
-    #         priority=priority,
-    #         match=match,
-    #         instructions=inst
-    #     )
-    #     datapath.send_msg(mod)
-    # @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    # def packet_in_handler(self, ev):
-    #     msg = ev.msg
-    #     datapath = msg.datapath
-    #     ofproto = datapath.ofproto
-
-    #     pkt = packet.Packet(msg.data)
-    #     eth = pkt.get_protocol(ethernet.ethernet)
-
-    #     # Log MAC addresses to help with debugging
-    #     self.logger.info(f"Source MAC: {eth.src}, Destination MAC: {eth.dst}")
-
-    #     # Count packets coming from host H3 on switch S1
-    #     if datapath.id == 1 and eth.src == '00:00:00:00:00:03':
-    #         self.packet_count += 1
-    #         self.logger.info(f"Packet count from H3 on switch S1: {self.packet_count}")
-    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+    def add_flow(self, datapath, priority, match, actions):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                             actions)]
-        if buffer_id:
-            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
-                                    priority=priority, match=match,
-                                    instructions=inst)
-        else:
-            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                    match=match, instructions=inst)
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(
+            datapath=datapath,
+            priority=priority,
+            match=match,
+            instructions=inst
+        )
         datapath.send_msg(mod)
-
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def _packet_in_handler(self, ev):
-        if ev.msg.msg_len < ev.msg.total_len:
-            self.logger.debug("packet truncated: only %s of %s bytes",
-                              ev.msg.msg_len, ev.msg.total_len)
+    def packet_in_handler(self, ev):
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-        in_port = msg.match['in_port']
 
         pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        eth = pkt.get_protocol(ethernet.ethernet)
 
-        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            return
-        dst = eth.dst
-        src = eth.src
+        # Log MAC addresses to help with debugging
+        self.logger.info(f"Source MAC: {eth.src}, Destination MAC: {eth.dst}")
 
-        dpid = datapath.id
-        self.mac_to_port.setdefault(dpid, {})
-
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
-        self.mac_to_port[dpid][src] = in_port
-
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
-        else:
-            out_port = ofproto.OFPP_FLOOD
-
-        actions = [parser.OFPActionOutput(out_port)]
-        if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
-            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-                return
-            else:
-                self.add_flow(datapath, 1, match, actions)
-        data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-            data = msg.data
-
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                  in_port=in_port, actions=actions, data=data)
-        datapath.send_msg(out)
-
+        # Count packets coming from host H3 on switch S1
+        if datapath.id == 1 and eth.src == '00:00:00:00:00:03':
+            self.packet_count += 1
+            self.logger.info(f"Packet count from H3 on switch S1: {self.packet_count}")
+    
