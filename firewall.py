916bb2831824b211d06ec3a6904002cfee410c4e
iitKD <valar5736@gmail.com>
2023-11-03 15:43:01 +0530

    firewall rules adding

--- firewall.py
+++ firewall.py
@@ -4,11 +4,7 @@ from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet, ethernet
-from ryu.lib import addrconv
-from ryu import cfg
-from ryu.controller import controller
-from ryu.lib import hub
-from ryu.topology import api as topo_api
+
 
 class FirewallMonitor(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -29,61 +25,52 @@ class FirewallMonitor(app_manager.RyuApp):
             eth_src='00:00:00:00:00:02',  # H2 MAC address
             eth_dst='00:00:00:00:00:05'   # H3 MAC address
         )
-        match_h3_to_h5 = parser.OFPMatch(
-            eth_src='00:00:00:00:00:03',  # H2 MAC address
-            eth_dst='00:00:00:00:00:05'   # H3 MAC address
+        match_h5_to_h2 = parser.OFPMatch(
+            eth_src='00:00:00:00:00:05',  # H2 MAC address
+            eth_dst='00:00:00:00:00:02'   # H3 MAC address
         )
 
-        match_h1_to_h4 = parser.OFPMatch(
-            eth_src='00:00:00:00:00:01',  # H1 MAC address
-            eth_dst='00:00:00:00:00:04'   # H4 MAC address
-        )
+        # match_h1_to_h4 = parser.OFPMatch(
+        #     eth_src='00:00:00:00:00:01',  # H1 MAC address
+        #     eth_dst='00:00:00:00:00:04'   # H4 MAC address
+        # )
 
         actions = []
 
         # Drop the packets that match the firewall rules
         self.add_flow(datapath, 0, match_h2_to_h5, actions)
-        self.add_flow(datapath, 0, match_h3_to_h5, actions)
-        self.add_flow(datapath, 0, match_h1_to_h4, actions)
-    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
+        self.add_flow(datapath, 0, match_h5_to_h2, actions)
+        #self.add_flow(datapath, 1, match_h1_to_h4, actions)
 
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                             actions)]
-        if buffer_id:
-            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
-                                    priority=priority, match=match,
-                                    instructions=inst)
-        else:
-            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                    match=match, instructions=inst)
-        datapath.send_msg(mod)
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
-        if ev.msg.msg_len < ev.msg.total_len:
-            self.logger.debug("packet truncated: only %s of %s bytes",ev.msg.msg_len, ev.msg.total_len)
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-        in_port = msg.match['in_port']
 
         pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        eth = pkt.get_protocol(ethernet.ethernet)
 
-        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            return
-        self.logger.info("packet in %s %s %s %s", datapath.id, eth.src, eth.dst, in_port)
-        out_port = ofproto.OFPP_FLOOD
-        actions = [parser.OFPActionOutput(out_port)]
-        data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-            data = msg.data
+        # Log MAC addresses to help with debugging
+        self.logger.info(f"Source MAC: {eth.src}, Destination MAC: {eth.dst}")
 
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                  in_port=in_port, actions=actions, data=data)
-        datapath.send_msg(out)
+        # Count packets coming from host H3 on switch S1
+        if datapath.id == 1 and eth.src == '00:00:00:00:00:03':
+            self.packet_count += 1
+            self.logger.info(f"Packet count from H3 on switch S1: {self.packet_count}")
 
-    
+
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(
+            datapath=datapath,
+            priority=priority,
+            match=match,
+            instructions=inst
+        )
+        datapath.send_msg(mod)
 
