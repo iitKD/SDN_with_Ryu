53c668fbbcd744f1a701fa1260fc0bb65e218c3c
iitKD <valar5736@gmail.com>
2023-11-03 19:34:44 +0530

    back

--- firewall.py
+++ firewall.py
@@ -75,7 +75,7 @@ class SimpleSwitch13(app_manager.RyuApp):
             header = pkt.get_protocols(ipv4.ipv4)[0]
             destination = header.dst
             source = header.src
-        if (src,dst) in blocked_pairs:
+        if (source,destination) in blocked_pairs:
             self.logger.info("Dropping packets between %s and %s "  , source, destination )
             return
         if dpid ==1 and (in_port == 4):
@@ -87,21 +87,21 @@ class SimpleSwitch13(app_manager.RyuApp):
         self.logger.info("packet form Host: %s through swithch: %s on port: %s to host: %s" , src, dpid, in_port, dst )
 
         
-        self.mac_to_port[dpid][src] = in_port
+        # self.mac_to_port[dpid][src] = in_port
 
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
-        else:
-            out_port = ofproto.OFPP_FLOOD
+        # if dst in self.mac_to_port[dpid]:
+        #     out_port = self.mac_to_port[dpid][dst]
+        # else:
+        out_port = ofproto.OFPP_FLOOD
 
         actions = [parser.OFPActionOutput(out_port)]
-        if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
-            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-                return
-            else:
-                self.add_flow(datapath, 1, match, actions)
+        # if out_port != ofproto.OFPP_FLOOD:
+        #     match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+        #     if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+        #         self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+        #         return
+        #     else:
+        #         self.add_flow(datapath, 1, match, actions)
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
