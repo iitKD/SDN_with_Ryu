e99bd7cbd3403850860aee75ca855fcb68aec91b
iitKD <valar5736@gmail.com>
2023-11-03 22:16:25 +0530

    changes

--- firewall.py
+++ firewall.py
@@ -71,37 +71,26 @@ class SimpleSwitch13(app_manager.RyuApp):
         }
 
        
-        if pkt.get_protocols(ipv4.ipv4):
-            header = pkt.get_protocols(ipv4.ipv4)[0]
-            destination = header.dst
-            source = header.src
-        if (source,destination) in blocked_pairs:
-            self.logger.info("Dropping packets between %s and %s "  , source, destination )
-            return
-        if dpid ==1 and (in_port == 4):
+        if pkt.get_protocols(ipv4.ipv4): 
+            dest = pkt.get_protocols(ipv4.ipv4).dst
+            sou = pkt.get_protocols(ipv4.ipv4).src
+            if (sou, dest) in blocked_pairs:
+                self.logger.info("Dropping packets between %s and %s "  , sou, dest )
+                return
+        if dpid ==1 and in_port == 4:
             self.packet_counter += 1
-        print("packets from host 3 flowing through switch 1 is ",self.packet_counter)
-        print("-"*15)
+            print("packets from host 3 flowing through switch 1 is ",self.packet_counter)
+        
 
 
         self.logger.info("packet form Host: %s through swithch: %s on port: %s to host: %s" , src, dpid, in_port, dst )
 
         
-        # self.mac_to_port[dpid][src] = in_port
-
-        # if dst in self.mac_to_port[dpid]:
-        #     out_port = self.mac_to_port[dpid][dst]
-        # else:
+    
         out_port = ofproto.OFPP_FLOOD
 
         actions = [parser.OFPActionOutput(out_port)]
-        # if out_port != ofproto.OFPP_FLOOD:
-        #     match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
-        #     if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-        #         self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-        #         return
-        #     else:
-        #         self.add_flow(datapath, 1, match, actions)
+        
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
